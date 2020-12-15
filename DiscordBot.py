f9fb08dd5b371ab49739e685838d2032765032b8
Mngz <mngz636@gmail.com>
2020-12-15 09:30:26 +0200

    Update DiscordBot.py

--- DiscordBot.py
+++ DiscordBot.py
@@ -7,7 +7,7 @@ import urllib.parse
 import discord
 from dotenv import load_dotenv
 
-load_dotenv(verbose=True)
+load_dotenv()
 
 try:
     traduction = gettext.translation('discord_affiliatebot', localedir='locale')
